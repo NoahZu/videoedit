e6aadc091d3ad5531ef08bdd6595f55b74d853f4
zujinhao <smallzoo@foxmail.com>
2020-10-12 18:24:16 +0800

    init album

--- app/src/main/java/io/github/noahzu/videoedit/VideoEditApplication.kt
+++ app/src/main/java/io/github/noahzu/videoedit/VideoEditApplication.kt
@@ -2,6 +2,13 @@ package io.github.noahzu.videoedit
 
 import android.app.Application
 import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.yanzhenjie.album.Album
+import com.yanzhenjie.album.AlbumConfig
+import com.yanzhenjie.album.AlbumFile
+import com.yanzhenjie.album.AlbumLoader
+import java.util.*
 
 class VideoEditApplication : Application() {
 
@@ -14,5 +21,20 @@ class VideoEditApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        Album.initialize(
+            AlbumConfig.newBuilder(this)
+            .setAlbumLoader(object : AlbumLoader {
+                override fun load(imageView: ImageView, albumFile: AlbumFile?) {
+                    load(imageView, albumFile?.path)
+                }
+
+                override fun load(imageView: ImageView, url: String?) {
+                    Glide.with(imageView.context)
+                        .load(url)
+                        .into(imageView)
+                }
+            })
+            .setLocale(Locale.getDefault()).build())
     }
 }
